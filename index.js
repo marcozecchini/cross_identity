2547c4e3229b6f96e4b84d4c7df18df1604074ef
marcozecchini <zecchini@diag.uniroma1.it>
2022-09-16 14:42:54 +0200

    more stable identity

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Identity = require('./utils/identity');
-const identity = new Identity('http://127.0.0.1:8545');
+const { secretKey } = require( './secrets.json');
+const { INFURA_TESTNET_ENDPOINT, INFURA_MAINNET_ENDPOINT } = require( "./constant");
+const identity = new Identity('http://127.0.0.1:8545', secretKey);
+
 
 async function main() {
     
     await identity.initRegistry();
-    let address = await identity.createDID();
+    // let address = await identity.createDelegate();
     await identity.setAlias("https://myblog.blogging-host.example/home");
-    await identity.setAlias("prova"); // TODO dovrebbero esserci entrambi
-    console.log(await identity.readDID(address));
+    await identity.setAlias("http://example.com");
+    console.log(await identity.readDID(Identity.accountFrom));
 
 }
 
--- utils/identity.js
+++ utils/identity.js
@@ -1,5 +1,4 @@
 const { EthereumDIDRegistry } = require('ethr-did-registry');
-const { secretKey } = require( '../secrets.json');
 const DidRegistryContract = EthereumDIDRegistry;
 const Web3 = require('web3');
 const { asciiToHex, hexToAscii } = require( "web3-utils");
@@ -7,10 +6,7 @@ const { Resolver } = require('did-resolver');
 const { getResolver } = require('ethr-did-resolver');
 const { toBuffer } = require( "ethereumjs-util");
 
-
-const { INFURA_TESTNET_ENDPOINT, INFURA_MAINNET_ENDPOINT } = require( "../constant");
-
-module.exports = function (HTTP_URL) {
+module.exports = function (HTTP_URL, secretKey) {
     let DidReg;
     const web3 = new Web3(new Web3.providers.HttpProvider(HTTP_URL));
     const accountFrom = web3.eth.accounts.privateKeyToAccount(secretKey);
@@ -37,27 +33,21 @@ module.exports = function (HTTP_URL) {
         return createReceipt.contractAddress;
     }
 
-    this.createDID = async function (){
-
-      const b32 = asciiToHex("did/pub/Ed25519/veriKey/hex");
-      const s = `${accountFrom.address}`
-
+    this.createDelegate = async function (account){
+      const b32 = asciiToHex("did/pub/Ed25519/sigAuth/hex");
+      const s = `${account.address}`
       const bvalue = toBuffer(s);
-
-      const setAttribute = DidReg.methods.setAttribute(accountFrom.address, b32, bvalue, 30000000);
+      const setAttribute = DidReg.methods.setAttribute(account.address, b32, bvalue, 30000000);
       let gas = await setAttribute.estimateGas({from: accountFrom.address})
       let receipt = await setAttribute.send({from: accountFrom.address, gas: Math.trunc(gas*(2.5))});
-      return accountFrom.address;
+      return account.address;
   }
 
   this.setAlias = async function (alias) {
 
     const b32 = asciiToHex("did/alsoKnownAs");
-
     const s = `${alias}`
-
     const bvalue = toBuffer(asciiToHex(s));
-
     const setAttribute = DidReg.methods.setAttribute(accountFrom.address, b32, bvalue, 30000000);
     let gas = await setAttribute.estimateGas({from: accountFrom.address})
     let receipt = await setAttribute.send({from: accountFrom.address, gas: Math.trunc(gas*(2.5))});
@@ -77,17 +67,21 @@ module.exports = function (HTTP_URL) {
 
     const didDocument = await didResolver.resolve(`did:ethr:development:${accountFrom.address}`);
     delete Object.assign(didDocument, {['verificationMethod']: didDocument['publicKey'] })['publicKey'];
-    didDocument['verificationMethod'] = didDocument['verificationMethod'].slice(1);
+
+    // didDocument['verificationMethod'] = didDocument['verificationMethod'].slice(1);
+
     didDocument['alsoKnownAs'] = await readAlias(`${accountFrom.address}`);
     return didDocument;
   }
 
   async function readAlias(address) {
-    let resultEvent = await DidReg.getPastEvents("DIDAttributeChanged");
-    console.log(resultEvent.length);
+    let resultEvent = await DidReg.getPastEvents("DIDAttributeChanged", {
+      fromBlock: 0,
+      toBlock: 'latest'
+    });
+
     let finalResult = [];
     for (let i = 0; i <= resultEvent.length - 1; i++) {
-      // let partialResult = {};
       let name = hexToAscii(resultEvent[i].returnValues.name).split('/')[1];
       let value = hexToAscii(resultEvent[i].returnValues.value);
       if ( address == resultEvent[i].returnValues.identity 
